5b35efd81460adeaed7891d6adbb0eda0d2cf22a
Leo <leokoblence@gmail.com>
2020-11-10 15:37:42 -0500

    update app.py

---
+++ app.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template, redirect, url_for
+from flask_moment import Moment
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
+import os
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+
+app = Flask(__name__)
+application = app
+app.config['SECRET_KEY'] = 'hard to guess string'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///ingredients.db'
+
+moment = Moment(app)
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    return render_template('index.html')
